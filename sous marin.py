b4d9b551c3acdcc0a73b7346d4a6dcf5a1f32db1
kevintabou <94377788+kevintabou@users.noreply.github.com>
2021-12-15 13:38:29 +0100

    Add files via upload

---
+++ sous marin.py
@@ -0,0 +1,13 @@
+if __name__=='__sous marin__':
+    f = open("day 2.txt", "r")
+    data = f.read().split("\n")
+
+    print(data)
+
+position = 0
+profondeur = 0
+
+for i in range(0, len(data)):
+
+
+
